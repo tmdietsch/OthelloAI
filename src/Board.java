aaece79f5627f7c27aba9c863b4193249345be46
Timothy Dietsch <dietschtm18@gcc.edu>
2021-04-12 15:39:00 -0400

    Some work in class

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,28 @@
 
 public class Board {
 
+	private final int BOARD_SIZE = 8;
+	private final int WHITE = 0;
+	private final int BLACK = 1;
+	
+	private int[][] board;
+	
+	Board() {
+		
+		board = new int[BOARD_SIZE][BOARD_SIZE];
+		
+		//Starting positions
+		board[3][3] = WHITE;
+		board[3][4] = BLACK;
+		board[4][3] = BLACK;
+		board[4][4] = WHITE;
+		
+	}
+	
+	public void move(int width, int height, boolean isWhite) {
+		
+		board[width][height] = isWhite ? WHITE : BLACK;
+		
+	}
+	
 }
---
+++ src/Game.java
@@ -0,0 +1,28 @@
+
+public class Game {
+
+	private boolean isWhiteTurn;
+	private int totalBlack;
+	private int totalWhite;
+	
+	Game(Player p1, Player p2) {
+		
+		isWhiteTurn = false;
+		
+	}
+	
+	/**
+	 * 0 for tie
+	 * 1 for white win
+	 * 2 for black win
+	 * 
+	 * @return
+	 */
+	private int determineWinner() {
+		
+		
+		
+		return 0;
+	}
+	
+}
