d5ce270d415320c7d7a246e3aabeeb94f86b132a
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2021-04-28 17:55:19 -0400

    added minValue method, maxValue stub, and getActions helper method

--- src/AI.java
+++ src/AI.java
@@ -1,17 +1,45 @@
+import java.util.ArrayList;
 
 public class AI extends Controller {
 	
 	private int useHeuristic;
+	private final int MAX_DEPTH = 6;
+	private boolean smart;
 	
-	public AI(boolean isWhite, int useHeuristic) {
+	public AI(boolean isWhite, int useHeuristic, boolean isSmart) {
 		this.isWhite = isWhite;
 		this.useHeuristic = useHeuristic;
+		smart = isSmart;
 	}
 	
 	@Override
 	public void makeMove(Board b) {
 		
 		
+	}
+	
+	private int maxValue(Board state, int alpha, int beta, int currDepth) {
+		return 0;
+	}
+	
+	private int minValue(Board state, int alpha, int beta, int currDepth) {
+		if(currDepth == MAX_DEPTH) {
+			if(smart)
+				return countingHeuristic(state, false) + cornersHeuristic(state, false);
+			return countingHeuristic(state, false);
+		}
+		
+		int v = Integer.MAX_VALUE;
+		ArrayList<Board> actions = getActions(state, false);
+		
+		for(Board a: actions) {
+			v = Math.min(v, maxValue(a, alpha, beta, currDepth+1));
+			if(v <= alpha)
+				return v;
+			beta = Math.min(beta, v);
+		}
+		
+		return v;
 	}
 	
 	private int countingHeuristic(Board board, boolean isMax) {
@@ -102,4 +130,24 @@ public class AI extends Controller {
 		return h;
 	}
 
+	private ArrayList<Board> getActions(Board b, boolean isMax){
+		ArrayList<Board> actions = new ArrayList<Board>();
+		ArrayList<Integer[]> possibleActions = b.getValidMoves();
+		Board temp;
+		
+		for(Integer[] i: possibleActions) {
+			temp = new Board(b);
+			if(isMax) {
+				temp.move(i[0], i[1], isWhite);
+			}
+			else {
+				temp.move(i[0], i[1], !isWhite);
+			}
+			actions.add(temp);
+		}
+		
+		
+		return actions;
+	}
+	
 }
