577f3b5731cb643fa51e9264072a254087b7e9dc
Timothy Dietsch <dietschtm18@gcc.edu>
2021-04-30 23:10:12 -0400

    Commented Controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,11 @@
 
+//Is extended by Player and AI classes for simplicity
 abstract class Controller {
 	
+	//Which color does the controller have? (black or white)
 	protected boolean isWhite;
 	
+	//Basic move method
 	public abstract void makeMove(Board b);
 
 }
